8c8a22e2e867a1e819070eaaf3611008e2500359
Paul-Arthur THIERY <paul-arthur.thiery@isep.fr>
2018-05-29 11:44:24 +0200

    update routes

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/temperatures', temperatureRouter);
+app.use('/', temperatureRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
