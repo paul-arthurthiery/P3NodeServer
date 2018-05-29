9fe9cbe1df0ea2a384b77e60caebf7741cff20e3
Paul-Arthur THIERY <paul-arthur.thiery@isep.fr>
2018-05-29 19:24:48 +0200

    send error back to client

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use('/', temperatureRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.send(req);
 });
 
 // error handler
